79bc900aadcbe006e1a924bc330ec9e018473f4e
pythonbond <72248280+pythonbond@users.noreply.github.com>
2022-10-27 18:39:40 +0530

    Create guess.py

---
+++ guess.py
@@ -0,0 +1,17 @@
+import random
+print("welcome to guessy wessy. You have 5 chances.")
+chances=4
+n=random.randint(1,9)
+print("Guess a number between 0 and 10 not including 0 or 10: ")
+while chances <=5:
+    guess=int(input("Enter Your Guessy Wessy: - "))
+    if guess == n:
+        print("You Win")
+        
+    elif guess < n:
+        print("go lower than ", guess)
+    else:
+        print("go higher than ", guess)
+    chances=chances-1
+    if chances==1:
+        print("you are a loser. how can you not figure it out. The number is ", n," Have basic logic you little idiot")
