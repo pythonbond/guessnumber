d797d794e864c677367b4c17b4a5597b7ed9349e
arunsharma00 <69965026+arunsharma00@users.noreply.github.com>
2022-10-27 19:03:29 +0530

    Changes on Line 3 and Line 10
    I have added chances=4 on line 3 and a break statement on line 10
    
    Please accept my PR

--- guess.py
+++ guess.py
@@ -1,13 +1,13 @@
 import random
 print("welcome to guessy wessy. You have 5 chances.")
-chances=4
+chances=5
 n=random.randint(1,9)
 print("Guess a number between 0 and 10 not including 0 or 10: ")
 while chances <=5:
     guess=int(input("Enter Your Guessy Wessy: - "))
     if guess == n:
         print("You Win")
-        
+        break
     elif guess < n:
         print("go lower than ", guess)
     else:
